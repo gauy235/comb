6f1e90c759d187a4976b3ab33628589181be6660
gauy235 <gauy235@gmail.com>
2023-10-28 23:03:59 +0800

    t4

--- EnMapper.java
+++ EnMapper.java
@@ -1,11 +1,9 @@
 package lgpl3.recycle;
 
-import java.util.LinkedHashMap;
-
 /**
  * The English map.<br/>
  *
- * @version 2023/10/17_19:20:00<br/>
+ * @version 2023/10/27_19:20:00<br/>
  *          <a target="_blank" href="http://c64.tw/w20/o/srchSrc.jsp?nameOfClass=EnMapper" >src</a>
  *
  */
@@ -13,260 +11,256 @@ public class EnMapper {
 
 	// TreeMap<String, String> M = new TreeMap<String, String>(String.CASE_INSENSITIVE_ORDER); //important
 	// public static TreeMap<String, String> M = new TreeMap<String, String>(Collator.getInstance(Locale.SIMPLIFIED_CHINESE));
-	public static LinkedHashMap<String, String> M = new LinkedHashMap<String, String>();
+	// public static LinkedHashMap<String, String> M = new LinkedHashMap<>();
 
-	static {
+	// entrusted to contribute 委託
+	// feasibility 可行性
+	// HealthBureau 衛生局
 
-		// entrusted to contribute 委託
-		// feasibility 可行性
+	// nosy 愛管閒事
+	// punct 刺; 點 (point)
+	// punctual 準時的
+	// wronged 寃枉
+	// adhere 堅持; 緊黏
 
-		M.put("HealthBureau", "衛生局");
+	// modest 謙遜的
+	// obstacle 障礙
+	// objective 客觀的;如實的;無偏見的
+	// tress 一束頭髮
+	// distressed 苦惱的
 
-		M.put("nosy", "愛管閒事");
-		M.put("punct", "刺; 點 (point)");
-		M.put("punctual", "準時的");
-		M.put("wronged", "寃枉");
-		M.put("adhere", "堅持; 緊黏");
+	// sorceress 女巫師
+	// rehearsal 演練; 排演
+	// provocation 挑釁
+	// vaunt 自吹自擂
+	// quarantine 檢疫隔離
 
-		M.put("modest", "謙遜的");
-		M.put("obstacle", "障礙");
-		M.put("objective", "客觀的;如實的;無偏見的");
-		M.put("tress", "一束頭髮");
-		M.put("distressed", "苦惱的");
+	// prohibit from 禁止 forbid
+	// disclaimer 免責聲明書
+	// cabinet 小柜子
+	// vet 獸醫;veterinary; 調查; 檢查
+	// vat 缸
 
-		M.put("sorceress", "女巫師");
-		M.put("rehearsal", "演練; 排演");
-		M.put("provocation", "挑釁");
-		M.put("vaunt", "自吹自擂");
-		M.put("quarantine", "檢疫隔離");
+	// keeping him up 使人不睡覺
+	// trait 特徵
+	// put up with 忍受 endure
+	// on the up (and up) 誠實的; 不斷向上的
+	// radical 游離基; 激進分子
 
-		M.put("prohibit from", "禁止 forbid");
-		M.put("disclaimer", "免責聲明書");
-		M.put("cabinet", "小柜子");
-		M.put("vet", "獸醫;veterinary; 調查; 檢查");
-		M.put("vat", "缸");
+	// discretion 自由裁量權
+	// repulsive 令人反感的
+	// envision 想象;展望
+	// shriek 尖叫
+	// coherent 連貫的;有條理的
 
-		M.put("keeping him up", "使人不睡覺");
-		M.put("trait", "特徵");
-		M.put("put up with", "忍受 endure");
-		M.put("on the up (and up)", "誠實的; 不斷向上的");
-		M.put("radical", "游離基; 激進分子");
+	// champ 冠軍; champion
+	// gulp 大口吞吃; 大口吸氣
+	// trickle 細流
+	// Rock paper scissors 剪刀, 石頭, 布
+	// recognise 承認; 認出
 
-		M.put("discretion", "自由裁量權");
-		M.put("repulsive", "令人反感的");
-		M.put("envision", "想象;展望");
-		M.put("shriek", "尖叫");
-		M.put("coherent", "連貫的;有條理的");
+	// pursue 追求
+	// revised 訂正過的
+	// reside 居住
+	// endorse 贊同
+	// defy 違抗; defied
 
-		M.put("champ", "冠軍; champion");
-		M.put("gulp", "大口吞吃; 大口吸氣");
-		M.put("trickle", "細流");
-		M.put("Rock paper scissors", "剪刀, 石頭, 布");
-		M.put("recognise", "承認; 認出");
+	// lie with sb. 某人的義務
+	// predestined 注定的
+	// pedestrian 行人
+	// retreat 撤退
+	// continental spa 大陸溫泉勝地
 
-		M.put("pursue", "追求");
-		M.put("revised", "訂正過的");
-		M.put("reside", "居住");
-		M.put("endorse", "贊同");
-		M.put("defy", "違抗; defied");
+	// itinerary 旅行指南
+	// unevenly 不均勻地
+	// virtue 美德
+	// affliction 折磨;痛苦
+	// superficial 膚淺
 
-		M.put("lie with sb.", "某人的義務");
-		M.put("predestined", "注定的");
-		M.put("pedestrian", "行人");
-		M.put("retreat", "撤退");
-		M.put("continental spa", "大陸溫泉勝地");
+	// unwind 放松
+	// hazard 潛在危險
+	// fragile 易碎品; 脆弱的
+	// satiric 諷刺的
+	// crux 癥結; 南十字座
 
-		M.put("itinerary", "旅行指南");
-		M.put("unevenly", "不均勻地");
-		M.put("virtue", "美德");
-		M.put("affliction", "折磨;痛苦");
-		M.put("superficial", "膚淺");
+	// irrevocably 不可撤銷地
+	// lenient 寬容的
+	// paradox 悖論
+	// enigma 謎
+	// abrupt 突兀的
 
-		M.put("unwind", "放松");
-		M.put("hazard", "潛在危險");
-		M.put("fragile", "易碎品; 脆弱的");
-		M.put("satiric", "諷刺的");
-		M.put("crux", "癥結; 南十字座");
+	// sprain 扭傷
+	// emigrate 移民
+	// intact 完好的
+	// hatch 孵化
+	// drastic 劇烈的
 
-		M.put("irrevocably", "不可撤銷地");
-		M.put("lenient", "寬容的");
-		M.put("paradox", "悖論");
-		M.put("enigma", "謎");
-		M.put("abrupt", "突兀的");
+	// appoint 任命
+	// courtesy 禮貌
+	// deceive 欺騙
+	// grief 悲痛
+	// cobweb 蜘蛛網
 
-		M.put("sprain", "扭傷");
-		M.put("emigrate", "移民");
-		M.put("intact", "完好的");
-		M.put("hatch", "孵化");
-		M.put("drastic", "劇烈的");
+	// compulsive 強迫性的
+	// sedentary 久坐的
+	// evade 逃避
+	// remedy 補救 (辦法)
+	// mundane 平凡的
 
-		M.put("appoint", "任命");
-		M.put("courtesy", "禮貌");
-		M.put("deceive", "欺騙");
-		M.put("grief", "悲痛");
-		M.put("cobweb", "蜘蛛網");
+	// repetitive 重複的
+	// solitary 獨居的
+	// symptom 症狀
+	// canal 運河
+	// solidarity 相挺; 團結一致
 
-		M.put("compulsive", "強迫性的");
-		M.put("sedentary", "久坐的");
-		M.put("evade", "逃避");
-		M.put("remedy", "補救 (辦法)");
-		M.put("mundane", "平凡的");
+	// cope 應付
+	// while 當；雖然
+	// resist 抵抗
+	// nurture 培育
+	// immerse 沉浸
 
-		M.put("repetitive", "重複的");
-		M.put("solitary", "獨居的");
-		M.put("symptom", "症狀");
-		M.put("canal", "運河");
-		M.put("solidarity", "相挺; 團結一致");
+	// hygiene 衛生
+	// hollow 空洞
+	// disturb 擾亂
+	// resistant 抵抗
+	// accomplish 完成
 
-		M.put("cope", "應付");
-		M.put("while", "當；雖然");
-		M.put("resist", "抵抗");
-		M.put("nurture", "培育");
-		M.put("immerse", "沉浸");
+	// abolish 廢除
+	// neutral 中立的
+	// obese 肥胖的
+	// ball pen 原子筆
+	// liquid paper 立可白
 
-		M.put("hygiene", "衛生");
-		M.put("hollow", "空洞");
-		M.put("disturb", "擾亂");
-		M.put("resistant", "抵抗");
-		M.put("accomplish", "完成");
+	// lie, lay, lain, lying 躺
+	// lie, lied, lied, lying 說謊
+	// lay, laid, laid, laying 放置於; 下蛋
+	// lie in 位於;睡懶覺;分娩
+	// cope 應付
 
-		M.put("abolish", "廢除");
-		M.put("neutral", "中立的");
-		M.put("obese", "肥胖的");
-		M.put("ball pen", "原子筆");
-		M.put("liquid paper", "立可白");
+	// injured 受傷的
+	// ambulance 救護車; 野戰醫院
+	// carry out 實行完成
+	// smuggle 走私
+	// confiscate 沒收
 
-		M.put("lie, lay, lain, lying", "躺");
-		M.put("lie, lied, lied, lying", "說謊");
-		M.put("lay, laid, laid, laying", "放置於; 下蛋");
-		M.put("lie in", "位於;睡懶覺;分娩");
-		M.put("cope", "應付");
+	// shush 噓
+	// cope 應付
+	// nuisance 討厭的人, 或事物
+	// imperative 勢在必行的
+	// eligible 合格的
 
-		M.put("injured", "受傷的");
-		M.put("ambulance", "救護車; 野戰醫院");
-		M.put("carry out", "實行完成");
-		M.put("smuggle", "走私");
-		M.put("confiscate", "沒收");
+	// obligate 有義務的
+	// obliged 強制,施恩;有義務的
+	// irritate 刺激
+	// skeptical 懷疑的;多疑的
+	// feasible 可行的
 
-		M.put("shush", "噓");
-		M.put("cope", "應付");
-		M.put("nuisance", "討厭的人, 或事物");
-		M.put("imperative", "勢在必行的");
-		M.put("eligible", "合格的");
+	// compromise 妥協
+	// promise 承諾
+	// comprehend 理解
+	// apprehend 理解; 逮捕
+	// arrest 逮捕
 
-		M.put("obligate", "有義務的");
-		M.put("obliged", "強制,施恩;有義務的");
-		M.put("irritate", "刺激");
-		M.put("skeptical", "懷疑的;多疑的");
-		M.put("feasible", "可行的");
+	// riotous 暴亂的;五彩斑斕的
+	// coral reef 珊瑚礁
+	// fatigue 疲勞
+	// protest demonstration 示威遊行
+	// march 遊行
 
-		M.put("compromise", "妥協");
-		M.put("promise", "承諾");
-		M.put("comprehend", "理解");
-		M.put("apprehend", "理解; 逮捕");
-		M.put("arrest", "逮捕");
+	// impose on 強加於
+	// bureaucracy 官僚
+	// tyrant 獨裁者
+	// autocrat;dictator 獨裁者
+	// gravel 碎石子
 
-		M.put("riotous", "暴亂的;五彩斑斕的");
-		M.put("coral reef", "珊瑚礁");
-		M.put("fatigue", "疲勞");
-		M.put("protest demonstration", "示威遊行");
-		M.put("march", "遊行");
+	// afford 負擔得起
+	// cognitive impairment 认知障碍
+	// intimate 密友;親密的
+	// insatiable 貪得無厭的
+	// follow suit 仿效;起而效尤
 
-		M.put("impose on", "強加於");
-		M.put("bureaucracy", "官僚");
-		M.put("tyrant", "獨裁者");
-		M.put("autocrat;dictator", "獨裁者");
-		M.put("gravel", "碎石子");
+	// contagious 接觸傳染
+	// infectious 空氣傳染
+	// council 議會
+	// carry on 开展;举行;进行
+	// carry on with 继续;坚持
 
-		M.put("afford", "負擔得起");
-		M.put("cognitive impairment", "认知障碍");
-		M.put("intimate", "密友;親密的");
-		M.put("insatiable", "貪得無厭的");
-		M.put("follow suit", "仿效;起而效尤");
+	// vulnerable 脆弱的
+	// credulous 易上當的
+	// intelligence officer 情報員
+	// be abundant with 充滿
+	// novice 新手
 
-		M.put("contagious", "接觸傳染");
-		M.put("infectious", "空氣傳染");
-		M.put("council", "議會");
-		M.put("carry on", "开展;举行;进行");
-		M.put("carry on with", "继续;坚持");
+	// fugitive 逃犯
+	// facial recognition 人臉辨識
+	// cathedral 大教堂
+	// panorama 全景圖
+	// sketch 素描
 
-		M.put("vulnerable", "脆弱的");
-		M.put("credulous", "易上當的");
-		M.put("intelligence officer", "情報員");
-		M.put("be abundant with", "充滿");
-		M.put("novice", "新手");
+	// acrobatic 特技表演的
+	// spectacle 壯麗
+	// reconciled 和解的
+	// speculate 推測
+	// fabricate 偽造
 
-		M.put("fugitive", "逃犯");
-		M.put("facial recognition", "人臉辨識");
-		M.put("cathedral", "大教堂");
-		M.put("panorama", "全景圖");
-		M.put("sketch", "素描");
+	// boycott 抵制
+	// escalate 逐步升級
+	// lofty 崇高的
+	// superstition 迷信
+	// stand her up 放鴿子
 
-		M.put("acrobatic", "特技表演的");
-		M.put("spectacle", "壯麗");
-		M.put("reconciled", "和解的");
-		M.put("speculate", "推測");
-		M.put("fabricate", "偽造");
+	// put somebody in his place 使某人安分
+	// at somebody's feet 臣服於
+	// cough 咳嗽
+	// cracka opposite of nigger
+	// execrable 糟糕的
 
-		M.put("boycott", "抵制");
-		M.put("escalate", "逐步升級");
-		M.put("lofty", "崇高的");
-		M.put("superstition", "迷信");
-		M.put("stand her up", "放鴿子");
+	// contaminate 汙染
+	// detrimental 有害的
+	// brewer 釀酒者
+	// undermine 削弱權威
+	// counterpart 相對應者
 
-		M.put("put somebody in his place", "使某人安分");
-		M.put("at somebody's feet", "臣服於");
-		M.put("cough", "咳嗽");
-		M.put("cracka", "opposite of nigger");
-		M.put("execrable", "糟糕的");
+	// eloquent 雄辯的
+	// frankly 坦白說
+	// workforce 勞動力
+	// debauchery 沉湎酒色
+	// charming 迷人的
 
-		M.put("contaminate", "汙染");
-		M.put("detrimental", "有害的");
-		M.put("brewer", "釀酒者");
-		M.put("undermine", "削弱權威");
-		M.put("counterpart", "相對應者");
+	// gasp 喘著气
+	// hesitation 猶豫
+	// fierce 猛烈的
+	// incredible 難以置信的
+	// coveted 夢寐以求的
 
-		M.put("eloquent", "雄辯的");
-		M.put("frankly", "坦白說");
-		M.put("workforce", "勞動力");
-		M.put("debauchery", "沉湎酒色");
-		M.put("charming", "迷人的");
+	// conventional 傳統的
+	// hear from 收到某人來信
+	// hear of 聽過陶喆這歌手
+	// fascinate 強烈地吸引
+	// melancholy 憂鬱的;沒人可憐你
 
-		M.put("gasp", "喘著气");
-		M.put("hesitation", "猶豫");
-		M.put("fierce", "猛烈的");
-		M.put("incredible", "難以置信的");
-		M.put("coveted", "夢寐以求的");
+	// banquet 盛宴
+	// bouquet 花束
+	// reciprocal 互惠的
+	// campaign 運動;戰役
+	// inherent 與生俱來的
 
-		M.put("conventional", "傳統的");
-		M.put("hear from", "收到某人來信");
-		M.put("hear of", "聽過陶喆這歌手");
-		M.put("fascinate", "強烈地吸引");
-		M.put("melancholy", "憂鬱的;沒人可憐你");
+	// reluctant 不情願的
+	// dogmatic 武斷的;自以為是的
+	// courteous 有禮貌的
+	// prudent 謹慎的
+	// monotonous 單調乏味的
 
-		M.put("banquet", "盛宴");
-		M.put("bouquet", "花束");
-		M.put("reciprocal", "互惠的");
-		M.put("campaign", "運動;戰役");
-		M.put("inherent", "與生俱來的");
+	// advice 忠告;不可數
+	// marble 大理石
+	// deliberate 仔細考慮;故意的
+	// merchandise 商品 commodity
+	// sundress 无袖背心裙
 
-		M.put("reluctant", "不情願的");
-		M.put("dogmatic", "武斷的;自以為是的");
-		M.put("courteous", "有禮貌的");
-		M.put("prudent", "謹慎的");
-		M.put("monotonous", "單調乏味的");
+	// abolish
+	// ab = abs: 表示 away from 「遠離」
+	// ol 其實就是 old，表示「老的或舊的」
+	// lish = 完成
+	// 這個單字所代表的含意是「讓舊的東西離開」而得出 廢除 的意思
 
-		M.put("advice", "忠告;不可數");
-		M.put("marble", "大理石");
-		M.put("deliberate", "仔細考慮;故意的");
-		M.put("merchandise", "商品 commodity");
-		M.put("sundress", "无袖背心裙");
-
-		// abolish
-		// ab = abs: 表示 away from 「遠離」
-		// ol 其實就是 old，表示「老的或舊的」
-		// lish = 完成
-		// 這個單字所代表的含意是「讓舊的東西離開」而得出 廢除 的意思
-
-	}
 }
